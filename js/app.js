c91f71562d368aedfdd8ed9c5541917358e3ed44
Adam26114 <86336270+Adam26114@users.noreply.github.com>
2022-06-23 10:01:41 +0630

    23 june 2022

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
 // console.log('hey');
 $(document).ready(function(){
-    console.log('hi');
+    // console.log('hi');
+
+    //Start Header
+
+        //Start nav
+
+            $('.navbuttons').click(function(){
+                $('.navbuttons').toggleClass('crossxs');
+            } );
+                
+           //for nav
+           $(window).scoll(function(){
+            //    console.log('hay');
+                let getscolltop = $(this).scollTop();
+
+
+                if(getscolltop >= 200){
+                    $('.navbar').addClass('navmenus');
+                }else{
+                    $('.navbar').removeClass('navmenus');
+                }
+           });
+
+        //End nav
+
+    //End Header
 });
