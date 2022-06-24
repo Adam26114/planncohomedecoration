7bffccc5d161c713c5a371dc3f2bfed3e03832a4
Adam26114 <86336270+Adam26114@users.noreply.github.com>
2022-06-24 10:19:00 +0630

    24 june 2022

--- js/app.js
+++ js/app.js
@@ -26,4 +26,27 @@ $(document).ready(function(){
         //End nav
 
     //End Header
+
+    //Stat Properties
+    $('.propertylists').click(function(){
+        //fot active item
+
+        // $('this').addClass("activeitems");
+        // $('this').siblings().removeClass('activeitems');
+
+        $(this).addClass("activeitems").siblings().removeClass('activeitems');
+
+        //fot filter
+
+        let getattvalue = $(this).attr('data-filter');
+        // console.log(getattvalue);
+
+        if(getattvalue === 'all'){
+            $('.filters').show('slide',500);
+        }else{
+            $('.filters').not('.'+getattvalue).hide('slide',500);
+            $('.filters').filter('.'+getattvalue).show('slide',500);
+        }
+    });
+    //End properties 
 });
